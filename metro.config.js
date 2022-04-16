0bba8f51552878ffb44f982e40c6c4a17646ae5f
Vagner B.C <vagner.bcastro@gmail.com>
2022-04-16 10:26:27 -0300

    Estrutura inicial

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = {
+  ...getDefaultConfig(__dirname),
+  resolver: {
+    extraNodeModules: {
+      components: path.resolve(__dirname, "src/components"),
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
